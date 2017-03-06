8e573b4f0b9f57eb9a4e8a7667942cc2530bbadc
James Land <jamesland94@gmail.com>
2017-03-06 08:54:40 -0600

    First commit, contains NTL source code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Smooth-Numbers
 Cryptography Midterm Project 2017
+This repo uses NTL libraries and C++. A couple of references below:
+
+NTL Intro
+http://www.shoup.net/ntl/doc/tour-intro.html
+
+Installing/Building an NTL project on UNIX:
+http://www.shoup.net/ntl/doc/tour-unix.html
---
+++ ntl-10.3.0/include/NTL/wizard_log.h
@@ -0,0 +1,18 @@
+
+
+/***************************
+Basic Configuration Options:
+NTL_GMP_LIP
+
+Resolution of double-word type:
+unsigned long long
+
+Performance Options:
+NTL_FFT_BIGTAB
+NTL_FFT_LAZYMUL
+NTL_TBL_REM
+NTL_GF2X_ALTCODE
+NTL_GF2X_NOINLINE
+***************************/
+
+
