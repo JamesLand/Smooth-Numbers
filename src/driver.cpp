e19e860ca7b147bea42b884af43b4e3e4f032b29
U-JamesL-PC\JamesL <JamesL@JamesL-PC.(none)>
2017-03-06 12:37:43 -0600

    driver in ntl src folder

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <math.h>
+#include <NTL/ZZ.h>
+
+using namespace NTL;
+
+int currentA = 0;
+
+int getNextA()
+{
+	return 0;
+}
 
 int main()
 {
-
+	ZZ a(2L);
+	ZZ b = power(a, 400);
+	//ZZ c = power(2, 400);
+	std::cout << "Result: " << a << " " << b << "\n";
 	return 0;
 
 }
